8783283b9abcecb37a00f37495b457d8b4d7d3d3
Hao <35542546+wenhaoy-0428@users.noreply.github.com>
2023-04-04 20:04:55 +0800

    Fix in-retrievable data after refreshing page
    * Implement a GetUserContext API in the backend that fetches global context data to the frontend (#35)
    
    * Frontend now calls the above api when starts. Currently, only `numPending`, the number of pending reviews in included in the return data.
    
    * Submission in the review page now updates the numPending context
    
    Signed-off-by: wenhaoy-0428 <wenhaoy.0428@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import NavBarTop from "./components/nav-bar/NavBar-Top";
 import NavBarBottom from "./components/nav-bar/NavBar-Bottom";
 import { useLoaderData, Outlet } from "react-router-dom";
 import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
   // The data fetched from server including user Information and library
   const data = useLoaderData();
   // The number of unfinished reviews
-  const [pendingReviews, setPendingReviews] = useState(100);
+  const [pendingReviews, setPendingReviews] = useState(data["numPending"]);
+
   const userInfo = {
     name: "Ada",
     email: "ada@gmail.com",
@@ -17,10 +19,22 @@ function App() {
   return (
     <div className="App grid grid-rows-6 grid-flow-col items-start justify-items-center h-screen">
       <NavBarTop pendingReviews={pendingReviews} userInfo={userInfo} />
-      <Outlet />
+      <Outlet context={[setPendingReviews]} />
       <NavBarBottom pendingReviews={pendingReviews} />
     </div>
   );
+}
+
+export async function loadApp() {
+  try {
+    let response = await axios.get("/api/getUserContext/");
+    console.log(response);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+
+  return null;
 }
 
 export default App;
