e34b9a7646f2d89be8f0012a9f37c1d9960f0c32
hao <wenhaoy.0428@gmail.com>
2023-05-09 12:07:01 +0800

    Relocate 3DAssets and remove unnecessary code
    Signed-off-by: hao <wenhaoy.0428@gmail.com>

--- EngMemo/prod_settings.py
+++ EngMemo/prod_settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/build/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
