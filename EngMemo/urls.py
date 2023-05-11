18ebd7c81c0d63a8c4242138d3a81d5e90714d7e
wenhaoy-0428 <wenhaoy.0428@gmail.com>
2023-05-11 20:10:54 +0800

    Hide admin Page
    * Rewrite loginview that will redirect all request to 404 if the user is not authenticated

--- EngMemo/urls.py
+++ EngMemo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from .views import ReactIndex
+from .views import ReactIndex, AdminLoginView
 
 urlpatterns = [
     path('api/account/', include('account.urls')),
+    re_path(r'admin/login/*', AdminLoginView, name="login"),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     re_path(r'.*', ReactIndex.as_view(), name="react-index"),
--- EngMemo/views.py
+++ EngMemo/views.py
@@ -1,4 +1,14 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import HttpResponse
+
 
 class ReactIndex(TemplateView):
     template_name = "index.html"
+
+
+def AdminLoginView(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    return HttpResponse(status=404)
